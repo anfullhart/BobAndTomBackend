4e0af1d1c74a3c715869125243eeb385c7db6d14
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-06-08 22:44:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -713,6 +713,6 @@ app.post("/api/delete/artist", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
